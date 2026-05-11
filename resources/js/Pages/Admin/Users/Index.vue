844021a238bd80b645e2b2fefab0727ba044c0d9
MJ-2502 <mjhaja@thelewiscollege.edu.ph>
2026-05-11 19:24:54 +0800

    minor fix

--- resources/js/Pages/Admin/Users/Index.vue
+++ resources/js/Pages/Admin/Users/Index.vue
@@ -106,7 +106,7 @@ const confirmDelete = () => window.confirm('Delete this facilitator? This action
                                         class="ui-btn-secondary ui-btn-sm !px-2.5"
                                         title="View details"
                                     >
-                                        <i>Details</i>
+                                        <i class="bi bi-eye"></i>
                                     </button>
 
                                     <Link
